d8d9bd84c7aafdf694c338012c775dfca80bb234
guanwanxiao <guanwanxiao@mbse.com.cn>
2024-11-28 11:44:50 +0800

    feat：打包项目启动 electron 文件

--- apps/draw-client/dist-electron/main.js
+++ apps/draw-client/dist-electron/main.js
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, Menu, session } from "electron";
 import { join } from "path";
 const getPath = (...args) => {
   return join(new URL(".", import.meta.url).pathname, ...args);
@@ -9,16 +9,39 @@ function createWindow() {
     height: 600,
     webPreferences: {
       preload: getPath("./preload/index.js"),
-      nodeIntegration: true,
-      contextIsolation: false
+      nodeIntegration: false,
+      // 禁用 Node 集成
+      contextIsolation: true,
+      // 启用上下文隔离
+      sandbox: true
     }
   });
   console.log("process.env.VITE_DEV_SERVER_URL:", process.env.VITE_DEV_SERVER_URL);
-  if (process.env.VITE_DEV_SERVER_URL) {
-    win.loadURL(process.env.VITE_DEV_SERVER_URL);
-  } else {
-    win.loadFile(getPath("../dist/index.html"));
-  }
+  const localPath = getPath("../dist/index.html");
+  console.log("localPath:", `file://${localPath}`);
+  win.loadURL(`file://${localPath}`);
+  const template = [
+    {
+      label: "View",
+      submenu: [
+        {
+          label: "Reload",
+          accelerator: "CmdOrCtrl+R",
+          click: () => win.reload()
+        }
+      ]
+    }
+  ];
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+  win.webContents.openDevTools();
+  session.defaultSession.webRequest.onBeforeRequest({ urls: ["file://.*"] }, (details, callback) => {
+    if (details.url.startsWith("file:///")) {
+      callback({ cancel: false });
+    } else {
+      callback({});
+    }
+  });
 }
 app.whenReady().then(() => {
   createWindow();
@@ -27,6 +50,8 @@ app.whenReady().then(() => {
       createWindow();
     }
   });
+}).catch((error) => {
+  console.error("Failed to initialize app:", error);
 });
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
--- apps/draw-client/electron/main.js
+++ apps/draw-client/electron/main.js
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, session, Menu  } from 'electron';
 import { join } from 'path';
 
 const getPath = (...args) => {
@@ -10,16 +10,46 @@ function createWindow() {
     height: 600,
     webPreferences: {
       preload: getPath('./preload/index.js'),
-      nodeIntegration: true,
-      contextIsolation: false,
+      nodeIntegration: false, // 禁用 Node 集成
+      contextIsolation: true, // 启用上下文隔离
+       sandbox: true,
     },
   });
+  const localPath = getPath('../dist/index.html');
   console.log('process.env.VITE_DEV_SERVER_URL:', process.env.VITE_DEV_SERVER_URL)
   if (process.env.VITE_DEV_SERVER_URL) {
     win.loadURL(process.env.VITE_DEV_SERVER_URL);
   } else {
-    win.loadFile(getPath('../dist/index.html'));
+    console.log('localPath:',`file://${localPath}`)
+    win.loadURL(`file://${localPath}`);
   }
+// 创建菜单模板
+const template = [
+  {
+    label: 'View',
+    submenu: [
+      {
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click: () => win.reload(),
+      },
+    ],
+  },
+];
+
+// 创建菜单
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
+  // 打开开发者工具
+  win.webContents.openDevTools();
+  // 设置允许加载本地资源的权限
+  session.defaultSession.webRequest.onBeforeRequest({ urls: ['file://.*'] }, (details, callback) => {
+    if (details.url.startsWith('file:///')) {
+        callback({ cancel: false });
+    } else {
+        callback({});
+    }
+});
 }
 
 app.whenReady().then(() => {
@@ -30,7 +60,9 @@ app.whenReady().then(() => {
       createWindow();
     }
   });
-});
+}).catch((error) => {
+  console.error('Failed to initialize app:', error);
+});;
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
