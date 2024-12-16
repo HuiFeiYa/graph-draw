5c9697b0c43e17dfef871cb5496b523fed43faf8
guanwanxiao <guanwanxiao@mbse.com.cn>
2024-12-16 20:24:49 +0800

    feat: 添加 ws 基本逻辑

---
+++ services/src/utils/SocketServer/WsClient.ts
@@ -0,0 +1,40 @@
+import { Server } from 'ws';
+import { WsService } from './WsService';
+const messageCallbacks = {}
+export class WsClient {
+    clientId: string
+    constructor(public websocket: WebSocket, public socketService: WsService, public searchParams: URLSearchParams) {
+        // 监听来自客户端的消息
+        this.websocket.addEventListener('message', this.onMessage.bind(this));
+        this.websocket.addEventListener('close', this.onClose.bind(this));
+    
+        this.websocket.addEventListener('error', this.onError.bind(this));
+    
+      }
+
+      onMessage(messageEvent: MessageEvent) {
+        // console.log(messageEvent);
+        const data = JSON.parse(messageEvent.data);
+        console.log('received message', data);
+        const callback = messageCallbacks[data.type];
+        if (!callback) {
+          console.error('message type not found');
+        } else {
+          callback(this, data as any);
+        }
+      }
+      onClose(closeEvent: CloseEvent) {
+        console.log('websocket onClose');
+        this.socketService.removeClient(this);
+    
+      }
+    
+      sendJSON(obj: any) {
+        this.websocket.send(JSON.stringify(obj));
+      }
+      onError(e: Event) {
+        console.log('websocket error');
+        this.socketService.removeClient(this);
+    
+      }
+}
