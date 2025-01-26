fb9922cae77cd3245438c0909920a80585632aba
guanwanxiao <guanwanxiao@mbse.com.cn>
2025-01-26 16:59:10 +0800

    feat: 添加箭头

--- packages/graph/src/shape/CommonEdge.vue
+++ packages/graph/src/shape/CommonEdge.vue
@@ -1,17 +1,33 @@
 <script setup lang="ts">
-import { Shape } from '@hfdraw/types';
+import { Shape, ShapeKey } from '@hfdraw/types';
+import { PathBuilder } from '@hfdraw/elbow'
 import { ref, reactive, computed, watch } from 'vue';
 import { waypointUtil } from '../util/edgeUtil/WaypointUtil';
+import { Point } from '@hfdraw/elbow/util/common-type';
 const props = defineProps<{
   shape:Shape
 }>();
 const computedData = computed(() => {
 const shape = props.shape;
-  const svgPath = waypointUtil.getSvgPath(props.shape);
+  // const svgPath = waypointUtil.getSvgPath(props.shape);
+  const pathBuilder = new PathBuilder({
+        distance: 10,
+        size: 10,
+        angle: 45
+    });
+    const keyPoints: Point[] = props.shape.waypoint.map(item => {
+      return [item.x, item.y]
+    })
+   
+    const drawOptions = {hasEndArrow: false, hasStartArrow: false}
+    if (shape.shapeKey === ShapeKey.Association) {
+      drawOptions.hasEndArrow = true;
+    }
+    const pathData = pathBuilder.draw(keyPoints, drawOptions)
   const style = Object.assign({}, shape.style);
-  shape.svgPath = svgPath;
+
   return {
-    svgPath,
+    svgPath:pathData,
     style
   };
 });
@@ -22,6 +38,5 @@ const shape = props.shape;
       :stroke="computedData.style.strokeColor"
       :stroke-width="computedData.style.strokeWidth"
       fill="none"
-      stroke-linejoin="round"
     />
 </template>
